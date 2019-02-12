e3a521e0b36012a12e82b9c733c32a45b407cb57
Michael Kirsche <jnredotter@gmail.com>
2019-02-12 16:29:40 -0500

    Added initial simulated results plot

---
+++ PlotSimReads.py
@@ -0,0 +1,37 @@
+#
+# Code for plotting simulated data:
+#    Plots for each read (proportion of minimizers shared, length)
+#    Colors to correspond to whether or not the read is contained
+#
+import sys
+import matplotlib.pyplot as plt
+from matplotlib import colors as mcolors
+
+fn = sys.argv[1]
+
+cts = []
+lens = []
+colors = []
+
+color_options = ['mediumorchid', 'lightskyblue']
+
+with open(fn) as f:
+    for line in f:
+        tokens = line.split()
+        name = tokens[0]
+        ct = float(tokens[1])
+        category = int(tokens[2])
+        
+        cts.append(ct)
+        
+        splitname = name.split('_')
+        length = int(splitname[2]) - int(splitname[1])
+        lens.append(length)
+        
+        colors.append(color_options[category])
+        
+plt.scatter(cts, lens, c=colors, s = 1)
+plt.title('Simulated results')
+plt.xlabel('Max kmers shared')
+plt.ylabel('Read length')
+plt.show()
