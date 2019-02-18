f6a03638dc246c61310a24cf2b6aa1329334bfc9
Michael Kirsche <jnredotter@gmail.com>
2019-02-18 14:34:36 -0500

    Added script to convert simulated reads file to BED file

---
+++ SRS/ReadsToBed.java
@@ -0,0 +1,29 @@
+package SRS;
+
+import java.io.*;
+public class ReadsToBed {
+public static void main(String[] args) throws IOException
+{
+	if(args.length != 1)
+	{
+		System.out.println("Usage: java ReadsToBed <readsfile>);");
+		return;
+	}
+	
+	String fn = args[0];
+	ReadUtils.PeekableScanner input = new ReadUtils.PeekableScanner(new FileInputStream(new File(fn)));
+	ReadUtils.FileType ft = ReadUtils.getFileType(input);
+	
+	while(input.hasNext())
+	{
+		String name = ReadUtils.getName(input, ft);
+		String[] parts = name.split("_");
+		System.out.print("ref");
+		for(int i = 0; i<parts.length; i++)
+		{
+			System.out.print("\t" + parts[i]);
+		}
+		System.out.println();
+	}
+}
+}
